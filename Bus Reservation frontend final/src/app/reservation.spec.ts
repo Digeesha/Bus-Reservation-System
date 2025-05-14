7a12059bc70b6e8b1e7568f5f5617bff313b5a32
Digeesha <124707275+Digeesha@users.noreply.github.com>
2025-05-14 20:42:06 +0100

    Delete Bus Reservation frontend final directory

--- Bus Reservation frontend final/src/app/reservation.spec.ts
+++
@@ -1,8 +0,0 @@
-import { reservation } from './reservation';
-import {ReservationService} from "./reservation.service";
-
-describe('Reservation', () => {
-  it('should create an instance', () => {
-    expect(new reservation()).toBeTruthy();
-  });
-});
